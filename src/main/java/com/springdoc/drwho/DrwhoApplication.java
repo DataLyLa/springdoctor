365e8ba366a1cee8e08b509c127f48933a0a4b13
Lyndia Laurole <lyndia.laurole@gmail.com>
2023-06-19 14:08:41 +0200

    spring first

---
+++ src/main/java/com/springdoc/drwho/DrwhoApplication.java
@@ -0,0 +1,13 @@
+package com.springdoc.drwho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrwhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrwhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springdoc/drwho/DrwhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdoc.drwho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrwhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
